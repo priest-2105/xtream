38cbe0c7c7c5da3470ebae810578cba267b5bfed
priest-2105 <fawzybailey782@gmail.com>
2024-03-10 15:17:33 +0100

    movielist animation

--- src/component/movielist.jsx
+++ src/component/movielist.jsx
@@ -1,18 +1,76 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React  from 'react'
 
 
-const MovieList = (props) => {
+const MovieList = ({ showMovieList }) => {
 
+ 
 
     return (
     <>  
-    {props.movies.map((movie, index) => (
+    {/* {props.movies.map((movie, index) => (
       <div className="container-fluid d-flex justify-content-start">
             <img src={movie.Poster} alt={movie.Title} />
         </div>
-    ))}    
+    ))}     */}
+    <div className="movie--list-page--container">
+      
 
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}>
+
+    <div className="movie-list-each-bottom-description">
+      <h3>MOvie Title</h3>
+      <p>This is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio</p>
+    </div>
+ 
+     </div>  
+      
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}>  
+    <div className="movie-list-each-bottom-description">
+      <h3>MOvie TitleMOvie TitleMOvie TitleMOvie TitleMOvie Title</h3>
+      <p>This is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the moThis is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the moThis is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the moThis is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the moThis is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the moThis is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio</p>
+    </div> </div>  
+      
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}>  
+    <div className="movie-list-each-bottom-description">
+      <h3>MOvie Title</h3>
+      <p>This is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio</p>
+    </div> </div>  
+      
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}>   
+    <div className="movie-list-each-bottom-description">
+      <h3>MOvie Title</h3>
+      <p>This is the movie description This is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio
+      This is the movie descriptioThis is the movie descriptio</p>
+    </div></div>  
+      
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}> </div>  
+      
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}> </div>  
+      
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}> </div>  
+      
+    <div className={`movie-list-each ${showMovieList ? 'show' : ''}`}> </div>  
+      
+      
+      
+       </div>
 
     </>
   )
--- src/layout/home.jsx
+++ src/layout/home.jsx
@@ -1,12 +1,17 @@
 import Navbar from './navbar';
 import { TypeAnimation } from 'react-type-animation';
 // import MovableElements from '../component/moveable';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import gsap from 'gsap';
+import MovieList from '../component/movielist';
+
+
 
 const Home = () => {
 
   const wrapperRef = useRef(null);
+  const [showMovieList, setShowMovieList] = useState(false);
+
 
   useEffect(() => {
     const throttledMouseMoveHandler = (e) => {
@@ -30,12 +35,18 @@ const Home = () => {
   }, []);
 
 
+
+  const toggleMovieList = () => {
+    setShowMovieList(!showMovieList);
+    // toggleMovieList();
+  };
+
     return (
         <div>
 
           <div className="home-page--outer--container">
 
-          <svg class="home-page-arrows">
+          <svg class="home-page-arrows" onClick={toggleMovieList}>
               <path class="a1" d="M0 0 L30 32 L60 0"></path>
               <path class="a2" d="M0 20 L30 52 L60 20"></path>
               <path class="a3" d="M0 40 L30 72 L60 40"></path>
@@ -60,12 +71,35 @@ const Home = () => {
                 className='ms-3'
                 repeat={Infinity}
               /> on</h6>
-                <h1 className='xtream-hompeage-intro-animation'>Xtream</h1>
+                <div className="home-page-intro-text-container"> 
+                 <svg width="120" height="150"   viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.02439 10.5L163.198 227.999L0 404.786H36.75L179.588 249.961L295.024 404.786H420L248.735 175.085L400.601 10.5H363.915L232.345 153.059L126.064 10.5H1.02439ZM55.061 37.5991H112.491L366.027 377.622H308.598L55.061 37.5991Z" fill="url(#paint0_angular_281_117)"/>
+                <defs>
+                <radialGradient id="paint0_angular_281_117" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(210 207.643) rotate(90) scale(197.143 210)">
+                <stop stop-color="#D7D7D7"/>
+                <stop offset="0.0201114" stop-color="#A3A2A2"/>
+                <stop offset="0.200011" stop-color="#A3A2A2"/>
+                <stop offset="0.200111" stop-color="#C7C6C6"/>
+                <stop offset="0.355111" stop-color="#A3A2A2"/>
+                <stop offset="0.480111" stop-color="#A3A2A2"/>
+                <stop offset="0.590111" stop-color="#A3A2A2"/>
+                <stop offset="0.595111" stop-color="#A3A2A2"/>
+                <stop offset="0.775111" stop-color="#A3A1A1"/>
+                <stop offset="1" stop-color="#949292"/>
+                </radialGradient>
+                </defs>
+                </svg>
+                <h1 className='xtream-hompeage-intro-animation'> TREAM</h1>
+                  </div> 
             </div>
               
                 </div>  
                 <Navbar/>
                   
+                <div className={`movie-list-outer-container ${showMovieList ? 'show' : ''}`}>
+                <button type="button" className='close-movie-list-button' onClick={toggleMovieList}>button</button>
+                  <MovieList showMovieList={showMovieList} />
+                </div>
 
           </div>
                 

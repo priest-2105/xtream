3c900176dcd8699475b8c400c4148b52c2319f88
priest-2105 <fawzybailey782@gmail.com>
2024-09-01 06:49:48 +0100

    style init

--- super.js
+++ super.js
@@ -10,8 +10,40 @@ async function getMovies() {
 
     console.log(respData);
     
+    respData.results.forEach(movie => {
+        const img = document.createElement("img")
+        img.src = IMGPATH + "/" + movie.poster_path
+
+        document.body.appendChild(img)
+    });
 
     return respData
 }
 
 getMovies();
+
+
+
+
+
+
+// Get all cards
+const cards = document.querySelectorAll('.card');
+
+// Initialize current index
+let currentIndex = 0;
+
+// Function to update the slider
+function updateSlider(index) {
+    const cardWidth = cards[0].offsetWidth + 20; // Card width plus gap
+    const offset = index * cardWidth;
+    document.querySelector('.cards').style.transform = `translateX(-${offset}px)`;
+}
+
+// Add event listeners to cards for click
+cards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        currentIndex = index;
+        updateSlider(currentIndex);
+    });
+});
